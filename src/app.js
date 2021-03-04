fc7b1fd6bdd9f249eb2a4d74b348c7e5f3ead1be
Greimer29 <greimerabelguzman29@gmail.com>
2021-03-03 21:45:18 -0400

    Configurar que los archivos estáticos queden disponibles

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', (req, res) => {
+// obtiene la ruta del directorio publico donde se encuentran los elementos estaticos (css, js).
+var publicPath = path.join(__dirname, 'public');
+
+// Para que los archivos estaticos queden disponibles.
+app.use(express.static(publicPath));
+
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
