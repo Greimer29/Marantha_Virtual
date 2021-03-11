58b6c247c8695766ff26b63d9fb174a37cab2c6d
Greimer29 <greimerabelguzman29@gmail.com>
2021-03-10 22:13:59 -0400

    Nuevas configuraciones realizadas, settings, middlewares, routes, requires, static files

--- src/app.js
+++ src/app.js
@@ -1,15 +1,37 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
 
-// obtiene la ruta del directorio publico donde se encuentran los elementos estaticos (css, js).
-var publicPath = path.join(__dirname, 'public');
+const app = express();
 
-// Para que los archivos estaticos queden disponibles.
-app.use(express.static(publicPath));
+//importing routes
+const routerControler = require('./routes/router')
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//middlewares
+app.use(morgan('dev'));
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'maranatha'
+}, 'single'));
+
+//routes
+app.use('/', routerControler);
+
+
+//statics files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(app.get('port'), () => {
+    console.log('Servidor activo en el puerto 3000')
 });
-
-app.listen(3000, console.log('Servidor activo en el puerto 3000'));
